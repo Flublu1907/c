cb5cc70a9dbf5c232ca1b1dcc17aa3c4a913cb04
Flublu1907 <annoo_ege@hotmail.com>
2024-12-17 22:21:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menuItems = document.getElementById("menuItems");
+    if (menuItems.style.display === "flex") {
+        menuItems.style.display = "none";
+    } else {
+        menuItems.style.display = "flex";
+    }
+  }
